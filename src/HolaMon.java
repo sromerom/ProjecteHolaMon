c2bf6ea5d883fc2e7e359f3526e250835fe43dde
sromerom <sromerom@esliceu.net>
2018-12-14 20:29:53 +0100

    Afegit Scanner

--- src/HolaMon.java
+++ src/HolaMon.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class HolaMon {
     public static void main(String[] args) {
+        Scanner pa = new Scanner(System.in);
+        System.out.println("Introduiex una paraula");
+        String paraula = pa.nextLine();
+
+
         Altreclasse.printHola();
         Altreclasse.printAdeu();
-        Altreclasse.printElQueVulguis("Estic fent github");
+        Altreclasse.printElQueVulguis(paraula);
     }
 }
